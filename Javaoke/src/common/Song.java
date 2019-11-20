9fbb2ded8e7e581b071fa366ac073e8536aad950
Sacha Wanono <sacha.wanono@etu.univ-nantes.fr>
2019-11-20 17:01:05 +0100

    Ajout de Try/catch pour la création de l'objet music (sequencer)

--- Javaoke/src/common/Song.java
+++ Javaoke/src/common/Song.java
@@ -2,8 +2,12 @@ package common;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
+
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
 public class Song {
@@ -32,14 +36,20 @@ public class Song {
 
         File midiFile = new File(title + Constants.FileReading.EXT_MIDI);
 
+        // Exception for new File doesn't exist
+        // Testing the File object
         if(!midiFile.exists() || midiFile.isDirectory() || !midiFile.canRead()) {
             System.out.println("The title is incorrect or the file doesn't exist.");
             System.exit(1);
         }
 
-        music = MidiSystem.getSequencer();
-        music.setSequence(MidiSystem.getSequence(midiFile));
-        
+        try {
+            music = MidiSystem.getSequencer();
+            music.setSequence(MidiSystem.getSequence(midiFile));
+        }
+        catch (MidiUnavailableException | InvalidMidiDataException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
