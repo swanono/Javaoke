5ee3928be2f78c06a4a48d7504dfc6b3302c5e3d
Ulysse Guyon <ulysse.guyon@etu.univ-nantes.fr>
2019-11-21 16:47:23 +0100

    change clientkaraoke

--- Javaoke/src/clientpkg/ClientKaraoke.java
+++ Javaoke/src/clientpkg/ClientKaraoke.java
@@ -2,14 +2,15 @@ package clientpkg;
 
 public class ClientKaraoke extends Client {
 
+    String musicTitle;
+
     public ClientKaraoke(String musicTitle) {
-        
+        this.musicTitle = musicTitle;
     }
 
     @Override
     public void sendRequest() {
         // TODO Auto-generated method stub
-
     }
 
     @Override
