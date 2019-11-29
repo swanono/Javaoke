d658865143d5cf93f30d8c686312372269eec44d
Ulysse Guyon <ulysse.guyon@etu.univ-nantes.fr>
2019-11-29 20:34:40 +0100

    change to Integer in Stats

--- Javaoke/src/serverpkg/Stats.java
+++ Javaoke/src/serverpkg/Stats.java
@@ -4,30 +4,26 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Stats implements Serializable {
 
     private static final long serialVersionUID = -4879098827817165092L;
 
-    private Map<String, AtomicInteger> userRequest;
-    private Map<String, AtomicInteger> musicRequest;
+    private Map<String, Integer> userRequest;
+    private Map<String, Integer> musicRequest;
 
     public Stats() {
-        userRequest = new HashMap<String, AtomicInteger>();
-        musicRequest = new HashMap<String, AtomicInteger>();
+        userRequest = new HashMap<String, Integer>();
+        musicRequest = new HashMap<String, Integer>();
     }
 
-    private synchronized void addOne(Map<String, AtomicInteger> request, String key) {
-        request.putIfAbsent(key, new AtomicInteger(0));
-        request.computeIfPresent(key, (name, value) -> {
-            value.addAndGet(1);
-            return value;
-        });
+    private synchronized void addOne(Map<String, Integer> request, String key) {
+        request.putIfAbsent(key, new Integer(0));
+        request.computeIfPresent(key, (name, value) -> value + 1);
     }
 
-    private synchronized String getMax(Map<String, AtomicInteger> request) {
-        return Collections.max(request.entrySet(), (r1, r2) -> { return r1.getValue().get() - r2.getValue().get(); }).getKey();
+    private synchronized String getMax(Map<String, Integer> request) {
+        return Collections.max(request.entrySet(), (r1, r2) -> { return r1.getValue() - r2.getValue(); }).getKey();
     }
 
     public void addOneToUser(String userName) {
